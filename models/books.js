e3065898ea605e618cc87064a5ef6d6bb1e0aec0
yokoyama1130 <ryogotan@ezweb.ne.jp>
2026-09-06 14:52:02 +0900

    modelにはmongoDBのサーバー起動いらないんだった

--- models/books.js
+++ models/books.js
@@ -1,15 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-mongoose.connect('mongodb://localhost:27017/books')
-    .then(() => {
-        console.log("コネクションOK");
-    })
-    .catch((err) => {
-        console.error("コネクションエラー");
-        console.error(err);
-    });
-
 const bookSchema = new Schema({
     name: String,
     author: String,
