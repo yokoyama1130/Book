c320f8dcc7eac95b843d5df3f88cf2cd67e4caca
yokoyama1130 <ryogotan@ezweb.ne.jp>
2026-09-06 15:22:57 +0900

    viewディレクトリへのパスの指定

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 8081;
 const Book = require("./models/books");
+const path = require("path");
 
 const mongoose = require("mongoose");
 
@@ -15,6 +16,7 @@ mongoose.connect('mongodb://localhost:27017/books')
     });
 
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 // 一覧画面のルーティング
 app.get("/", async (req, res) => {
