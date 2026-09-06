4a169dae5d9dbd8ca3a33adbb5ab6a06f52328a3
yokoyama1130 <ryogotan@ezweb.ne.jp>
2026-09-06 15:47:04 +0900

    新規登録機能の追加

--- app.js
+++ app.js
@@ -18,10 +18,28 @@ mongoose.connect('mongodb://localhost:27017/books')
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+app.use(express.urlencoded({extended: true}));
+
 // 一覧画面のルーティング
 app.get("/", async (req, res) => {
     const books = await Book.find({});
     res.render("books/index", { books });
+});
+
+// 新規登録画面へのルーティング
+app.get("/new", (req, res) => {
+    res.render("books/new");
+});
+
+// 新規登録ロジック
+app.post("/book/new", async (req, res) => {
+    const book = await req.body.book;
+    const newBook = new Book({
+        name: book.name,
+        author: book.author
+    });
+    await newBook.save();
+    res.redirect("/");
 });
 
 app.listen(PORT, () => {
