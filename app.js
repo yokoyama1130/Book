700869639e178c690ea07fd56a6c22cb4ddf2d83
yokoyama1130 <ryogotan@ezweb.ne.jp>
2026-09-06 14:39:39 +0900

    mongoDBのサーバー起動

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ const express = require("express");
 const app = express();
 const PORT = 8081;
 
+const mongoose = require("mongoose");
+
+mongoose.connect('mongodb://localhost:27017/books')
+    .then(() => {
+        console.log("コネクションOK");
+    })
+    .catch((err) => {
+        console.error("コネクションエラー");
+        console.error(err);
+    });
+
 app.listen(PORT, () => {
     console.log("ポート8081でサーバー起動");
 });
